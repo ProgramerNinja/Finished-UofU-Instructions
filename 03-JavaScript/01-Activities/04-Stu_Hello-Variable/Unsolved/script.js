25353aa6549f32ae6b320edaf6f55141d706276f
ProgramerNinja <nohamcmjt@gmail.com>
2023-08-28 21:59:04 -0600

    Adding classwork for this monday

---
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,14 @@
+// Write Your JavaScript Code Here
+var personName="Jared";
+var pet="no";
+var funFact="I'm a nerd";
+
+console.log("My name is " + personName + ".");
+console.log("I have " + pet + " pets");
+console.log("Fun Fact: "+ funFact + ".");
+
+personName="Dana";
+pet="2 border calie and 2 cat";
+funFact="they used to own a wolf hybrid";
+
+console.log("My partner's name is " + personName + ". They have " + pet + " pets. A fun fact about them is " + funFact);
---
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,32 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+//This will print a number
+console.log(typeof one);
+//This will print a string 
+console.log(typeof two); 
+//This will print a boolean
+console.log(typeof three); 
+//This will print a number
+console.log(typeof four); 
+//This will print a string
+console.log(typeof five);
+//This will print a none/undefined
+console.log(typeof six);
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+//This will print a string
+console.log(typeof four);
+//This will print a boolean
+console.log(typeof five);
+//This will print a number
+console.log(typeof six);
