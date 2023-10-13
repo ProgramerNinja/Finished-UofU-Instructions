6165387fede3196b0502b09b90328e135910d8b5
ProgramerNinja <nohamcmjt@gmail.com>
2023-10-12 21:59:22 -0600

    adding todays work

--- 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,13 @@
 // music should be an object with title, artist, and album properties
 const music = {
-  // code here
- 
+  title: "The Search",
+  artist: "NF",
+  album: "The Search Album"
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = `The song you are listening to is ${music.title}!
+It was written by ${music.artist} and was a part of their album ${music.album}!`;
 
 console.log(songSnippet)
 
