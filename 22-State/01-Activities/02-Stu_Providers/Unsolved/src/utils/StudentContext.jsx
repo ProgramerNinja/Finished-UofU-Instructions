a5c38d450d4af4aaefa546366d7b0fd5731ba337
ProgramerNinja <nohamcmjt@gmail.com>
2024-01-25 21:13:29 -0700

    adding todays work

---
+++ 22-State/01-Activities/02-Stu_Providers/Unsolved/src/utils/StudentContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext } from 'react';
+
+// Initialize new context for students
+const StudentContext = createContext();
+
+// We create a custom hook to provide immediate usage of the student context in other components
+export const useStudentContext = () => useContext(StudentContext);
+
+// StudentProvider is a function that will return a provider component that makes global state available
+export const StudentProvider = ({ children }) => {
+  const initialState = {
+    students: [
+      // TODO: Add two students with a `name`, `major` and `id` property
+      {
+        name: "billy-ripped-jeans",
+        major: "not my lover",
+        id: 1,
+      },
+      {
+        name: "i thought",
+        major: "she was the",
+        id: 2,
+      },
+    ],
+  };
+
+  // TODO: Fill in the value prop for the provider
+  return (
+    <StudentContext.Provider value={initialState}>
+      {children}
+    </StudentContext.Provider>
+  );
+};
